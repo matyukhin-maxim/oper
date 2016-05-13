83e5a97f2d60890980d08570025a992e6fccee43
fellix <matuhin.maxim@gmail.com>
2016-05-13 16:37:40 +0900

    pretty view

--- status.php
+++ status.php
@@ -1,6 +1,10 @@
 <?php
     echo $_SERVER['COMPUTERNAME'] . "<br/>" . PHP_EOL;
     
-	var_dump($_SERVER);
+	//print_r($_SERVER);
     //phpinfo();
+	
+	foreach ($_SERVER as $key => $val) {
+		echo  "$key  =>  $val\n<br/>";
+	}
 ?>
